c989b0069c830d4cc2f9c9b96be9d82de98ecbf5
Joshua Trono <Josh.Trono@gmail.com>
2026-03-05 22:42:25 -0500

    test

--- Website/Program.cs
+++ Website/Program.cs
@@ -1,3 +1,5 @@
+using AspNetStatic;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -24,6 +26,5 @@ app.MapControllerRoute(
         name: "default",
         pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
